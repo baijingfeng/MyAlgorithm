0b9401ebd71b9a55425b190bce2b5696067655bd
bai.jingfeng <bai.jingfeng@qq.com>
2020-04-07 18:04:06 +0800

    update

--- example.js
+++ example.js
@@ -1,14 +1,39 @@
 /** 数组映射为对象 */
-var arr = [{a: 1, d: 4}, {b: 2}, {c:3}]
+function arrToObj(array) {
+	const arr = array || [{a: 1, d: 4}, {b: 2}, {c: 3}]
 
-var obj = arr.reduce((obj, item) => {
-  // Object.keys(item).forEach(key => obj[key] = item[key])
-  for (key in item) {
-    obj[key] = item[key]
-  }
-  return obj
-}, {})
+	const obj = arr.reduce((obj, item) => {
+		// Object.keys(item).forEach(key => obj[key] = item[key])
+		for (key in item) {
+			obj[key] = item[key]
+		}
+		return obj
+	}, {})
+	console.log({...obj})
+}
 
+/** find() */
 
-console.log([1,2])
-console.log({...obj})
+function selfFind(array) {
+	const arr = array || [1, 3, 4, 5, 8]
+
+	const result = arr.find((item, index) => index % 2 === 1 && item % 2 === 1)
+
+	console.log('result', result)
+
+	Array.prototype.selfFind = function (callback) {
+		for (let index = 0; index < this.length; index++) {
+			if (callback(this[index], index)) {
+				return this[index]
+			}
+		}
+	}
+
+	const myResult = arr.selfFind((item, index) => index % 2 === 1 && item % 2 === 1)
+
+	console.log('myResult', myResult)
+}
+
+// selfFind()
+
+/**  */
